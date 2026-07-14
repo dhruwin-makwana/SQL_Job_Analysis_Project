9b48151642c0ea203d102d7927fb7bb5d3494d45
dhruwin-makwana <108353180+dhruwin-makwana@users.noreply.github.com>
2026-07-14 22:59:12 +0530

    Optimal Skills Query for Data Analyst Jobs

---
+++ project_queries/5_optimal_skills.sql
@@ -0,0 +1,23 @@
+/* This query retrieves the top 25 most in-demand skills for the job title 'Data Analyst' based on the number of job postings and their average salary. */
+
+
+
+SELECT 
+    skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT(skills_job_dim.job_id) AS demand_count,
+    ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+GROUP BY
+    skills_dim.skill_id
+HAVING
+    COUNT(skills_job_dim.job_id) > 10
+ORDER BY
+    demand_count DESC,
+    avg_salary DESC
+LIMIT 25;
